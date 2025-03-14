9897e738ec8b8e1b71c089fe2f248077a2595c09
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-03-14 19:12:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def cargar_metadata():
         return jsonify({'error': 'Error interno del servidor'}), 500
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=4449, debug=True)
+app.run(debug=True)
