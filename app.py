29b386737523207cd81979308220c465931e2512
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-03-23 12:57:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask_cors import CORS
 from traductor import Traducir
 from metadata import Metadatos
 from buscador_imagenes import Buscador_Imagenes
+from buscador_textos import buscar_textos
+from duckduckgo_search import DDGS
+
 
 app = Flask(__name__)
 CORS(app)  # Esto habilita CORS para toda la app
 traductor = Traducir()  # Instancia correcta del traductor
 metadata = Metadatos()  # Instancia correcta de los metadatos
 buscador = Buscador_Imagenes()  # Instancia correcta del buscador de imágenes
+buscador_textos = buscar_textos()  # Instancia correcta del buscador de textos
 
 @app.route('/')
 def home():
@@ -69,6 +73,25 @@ def cargar_imagenes():
     except Exception as e:
         print(f"Error en el servidor: {e}")
         return jsonify({'error': 'Error interno del servidor'}), 500
+
+@app.route('/texto', methods=['GET'])
+def obtener_traduccion():
+    texto = request.args.get('texto')
+    
+    if not texto:
+        return jsonify({'error': 'Parámetro "texto" requerido'}), 400
+
+    resultados = buscar_textos(texto, max_resultados=5)
+    
+    if resultados:
+        print(f"Resultados de búsqueda para '{query}':\n")
+        for i, res in enumerate(resultados, 1):
+            print(f"Resultado {i}:")
+            print(f"Título: {res['titulo']}")
+            print(f"URL: {res['url']}")
+            print(f"Snippet: {res['snippet'][:15000]}...\n")  # Muestra primeros 150 caracteres
+    else:
+        print("No se encontraron resultados.")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
