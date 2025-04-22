bb2c3f89523c2347b5c220d3714ae0630fa70e38
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-04-22 12:41:17 -0300

    Update consulta_api.py

--- consulta_api.py
+++ consulta_api.py
@@ -1,1 +1,20 @@
-consulta_api
+import requests
+
+def obtener_radios(genero='blues'):
+    url = f'https://zeno.fm/api/stations/?query={genero}&limit=&genre=Music'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json(), 200
+    except requests.RequestException as e:
+        return {'error': str(e)}, 500
+
+def obtener_radio_por_id(radio_id):
+    url = f'https://zeno.fm/api/stations/{radio_id}/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json(), 200
+    except requests.RequestException as e:
+        return {'error': str(e)}, 500
+
