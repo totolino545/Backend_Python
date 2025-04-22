65aa76e7fb49d9f1559f92aab4bde9773726a5d0
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-04-22 18:35:59 -0300

    Update consulta_api.py

--- consulta_api.py
+++ consulta_api.py
@@ -18,3 +18,12 @@ def obtener_radio_por_id(radio_id):
     except requests.RequestException as e:
         return {'error': str(e)}, 500
 
+def obtener_metadata_por_id(id):
+    url = f'https://api.zeno.fm/mounts/metadata/subscribe/{id}/'
+    try:
+        response = requests.get(id)
+        response.raise_for_status()
+        return response.json(), 200
+    except requests.RequestException as e:
+        return {'error': str(e)}, 500
+
